da0d2b55f6fbc49b28481f4ee38689885d8b6b8b
jcallin <j.callin@hotmail.com>
2016-08-25 23:24:22 -0700

    "Updated the repo with new README and better script"

--- psuedosmart.py
+++ psuedosmart.py
@@ -7,19 +7,25 @@ dictionary = PyDictionary()
 origText = str(pyperclip.paste())
 finalWordList = []
 
-for word in origText.split():
-    if(len(word) > 5):
-        #print("replacing word " + word)
-        newWords = dictionary.synonym(word)
-        if (newWords == None):
+
+def pseudosmart():
+    for word in origText.split():
+        if (len(word) > 5):
+            # print("replacing word " + word)
+            newWords = dictionary.synonym(word)
+            if (newWords == None):
+                continue
+            newWord = random.choice(newWords)
+            newWord = newWord + " "
+            finalWordList.append(newWord)
             continue
-        newWord = random.choice(newWords)
-        newWord = newWord + " "
-        finalWordList.append(newWord)
-        continue
-    word = word + " "
-    finalWordList.append(word)
-finalText = ""
-for word in finalWordList:
-    finalText = finalText + word
-print(finalText)
+        word = word + " "
+        finalWordList.append(word)
+    finalText = ""
+    for word in finalWordList:
+        finalText = finalText + word
+    print(finalText)
+
+
+if __name__ == '__main__':
+    pseudosmart()
